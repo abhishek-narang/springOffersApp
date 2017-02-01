0ddd3bdb9b9ddf93b8acbf7fb5bbdfb123e7384d
Abhishek Narang <narang.abhishek90@gmail.com>
2017-02-01 17:02:17 +1100

    Final Version of the Offers Application made using Spring

---
+++ src/com/abhishek/spring/web/service/UsersService.java
@@ -0,0 +1,35 @@
+package com.abhishek.spring.web.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.abhishek.spring.web.dao.User;
+import com.abhishek.spring.web.dao.UsersDao;
+
+@Service("usersService")
+public class UsersService {
+	
+	private UsersDao usersDao;
+	
+	@Autowired
+	public void setOffersDao(UsersDao usersDao) {
+		this.usersDao = usersDao;
+	}
+
+	
+	public void create(User user) {
+		usersDao.create(user);
+	}
+
+
+	public boolean exists(String username) {
+		return usersDao.exists(username);
+	}
+
+
+	public List<User> getAllUsers() {
+		return usersDao.getAllUsers();
+	}
+}
